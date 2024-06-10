dfffe29a58625b1f9a506cf53a659df26c935bd9
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-06-10 20:46:08 +0300

    Updates to login procesw

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,44 @@
+"use client"
+
 import { Icons } from "@/components/Icons"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validators"
+import { trpc } from "@/trpc/client"
+import { toast } from "sonner"
 
 const Page = () => {
+
+  const { 
+    register,
+    handleSubmit, 
+    formState: {errors}
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  })
+
+  const {mutate, isLoading} = 
+    trpc.auth.createPayloadUser.useMutation({
+        onError: (err) => {
+            if(err.data?.code === "CONFLICT") {
+                toast.error('This email is already in use sign-in instead.')
+            }
+        }
+    })
+
+  const onSubmit = ({ 
+    email, 
+    password
+  }: TAuthCredentialsValidator) => { 
+    mutate({ email, password })
+  }
+
   return (
     <>
         <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
@@ -10,6 +48,46 @@ const Page = () => {
                     <h1 className="text-2xl font-bold">
                         Create an account
                     </h1>
+
+                    <Link 
+                        href='/sign-in'
+                        className={buttonVariants({
+                            variant: 'link',
+                            className: 'gap-1.5',
+                        })}
+                    >
+                        Already have an account? Sign in
+                        <ArrowRight className="h-4 w-4"/>
+                    </Link>
+                </div>
+
+                <div className="grid gap-6">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="grid gap-2">
+                            <div className="grid gap-1 py-2">
+                                <Label htmlFor='email'>Email</Label>
+                                <Input
+                                    {...register("email")} 
+                                    className={cn({
+                                        "focus-visible:ring-red-500": errors.email
+                                    })}
+                                    placeholder="you@example.com"
+                                />
+                            </div>
+                            <div className="grid gap-1 py-2">
+                                <Label htmlFor='password'>Password</Label>
+                                <Input 
+                                    {...register("password")}
+                                    className={cn({
+                                        "focus-visible:ring-red-500": errors.password
+                                    })}
+                                    placeholder="Password"
+                                />
+                            </div>
+
+                            <Button>Sign up</Button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
