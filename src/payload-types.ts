76b065a0aee66520e27a27648f6c617df37b0498
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-06-14 22:49:58 +0300

    Pre-debugging-1

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -24,7 +24,7 @@ export interface Config {
  */
 export interface User {
   id: string;
-  role: 'admin' | 'user';
+  role?: ('admin' | 'user') | null;
   updatedAt: string;
   createdAt: string;
   email: string;
