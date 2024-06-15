5238e9a8d6f468c917b161cc1b95e80fba5f0237
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-06-15 20:09:02 +0300

    Main and Resolution added for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                hostname: "localhost",
-                pathname: "**",
-                port: "3000",
-                protocol: "http",
-            },
+        domains: [
+            'localhost',
+            'digitalhippo-production-4d9e.up.railway.app',
         ],
     },
 };
